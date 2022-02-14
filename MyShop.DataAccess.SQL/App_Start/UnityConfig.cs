1af418f79c65387d9bab2496fa6032a7b92d92bc
Gowtham121097 <gowtham121097@gmail.com>
2022-02-14 21:15:33 +0530

    VG-Implemented SQL Repository

--- MyShop.DataAccess.SQL/App_Start/UnityConfig.cs
+++ MyShop.DataAccess.SQL/App_Start/UnityConfig.cs
@@ -1,12 +1,8 @@
-using MyShop.Core.Contracts;
-using MyShop.Core.Models;
-using MyShop.DataAccess.InMemory;
-using MyShop.DataAccess.SQL;
 using System;
 
 using Unity;
 
-namespace MyShop.WebUI
+namespace MyShop.DataAccess.SQL
 {
     /// <summary>
     /// Specifies the Unity configuration for the main container.
@@ -46,11 +42,6 @@ namespace MyShop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            //container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            //container.RegisterType<IRepository<ProductCaterogy>, InMemoryRepository<ProductCaterogy>>();
-
-            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
-            container.RegisterType<IRepository<ProductCaterogy>, SQLRepository<ProductCaterogy>>();
         }
     }
 }
